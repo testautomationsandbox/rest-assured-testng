be9b34df6dcc1adee8b588626daf390f2ef4835b
ftavora <ftavora@elementum.com>
2020-07-02 09:18:48 -0300

    base urls move to env vars

--- src/main/java/tech/fertavora/apitesting/clients/swapi/endpoints/FilmsEndpoint.java
+++ src/main/java/tech/fertavora/apitesting/clients/swapi/endpoints/FilmsEndpoint.java
@@ -1,11 +1,14 @@
 package tech.fertavora.apitesting.clients.swapi.endpoints;
 
 import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
 import tech.fertavora.apitesting.clients.swapi.SwapiService;
 
 import static io.restassured.RestAssured.given;
 
 public class FilmsEndpoint extends SwapiService {
+
+    protected static RequestSpecification filmsEndpoint = setEndpoint("/films", swapiServiceBaseSpec);
 
     public static ValidatableResponse getFilmById(int filmId) {
         return given()
--- src/main/java/tech/fertavora/apitesting/clients/swapi/endpoints/PeopleEndpoint.java
+++ src/main/java/tech/fertavora/apitesting/clients/swapi/endpoints/PeopleEndpoint.java
@@ -1,11 +1,14 @@
 package tech.fertavora.apitesting.clients.swapi.endpoints;
 
 import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
 import tech.fertavora.apitesting.clients.swapi.SwapiService;
 
 import static io.restassured.RestAssured.given;
 
 public class PeopleEndpoint extends SwapiService {
+
+    protected static RequestSpecification peopleEndpoint = setEndpoint("/people", swapiServiceBaseSpec);
 
     public static ValidatableResponse getPeopleById(int peopleId) {
         return given()
